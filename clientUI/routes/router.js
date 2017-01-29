6ee384b85c449c1acf44968502c560ba2199958f
genyrosk <e.roskach@gmail.com>
2017-01-29 12:00:20 +0000

    files and gifs now served dynamically

--- clientUI/routes/router.js
+++ clientUI/routes/router.js
@@ -1,41 +1,66 @@
+var request = require('request');
+var rp = require('request-promise');
+
 module.exports = (router) => {
 	router.get('/', (req, res) => {
 
-		var inventory = require('../../records/db.json').entries;
-
-		inventory = inventory.map( (item) => Object.assign(
-			{}, 
-			item, 
-			{ gifUrl: '/records/' + item.gifUrl }
-		));
-
-		res.render('pages/index', {
-			entries: inventory,
-			error: false,
-		});
+		rp('http://localhost:3000/files/db.json')
+			.then((json) => {
+				console.log(json);
+				var inventory = JSON.parse(json).entries.map( (item) => Object.assign(
+					{}, 
+					item, 
+					{ gifUrl: 'http://localhost:3000/files/' + item.gifUrl }
+				));
+				res.render('pages/index', {
+					entries: inventory,
+					error: false,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				res.render('pages/index', {
+					error: true,
+				});
+			});
 	});
 
 	router.get('/:name', (req, res) => {
-		var filePath = __dirname + '/../../records/' + req.params.name + '.json';
 
-		try {
-			var item = require(filePath);
-			item = Object.assign(
-				{}, 
-				item, 
-				{ gifUrl: '/records/' + item.gifUrl }
-			);
-			console.log('item', item);
-			res.render('pages/index', {
-				entries: [item],
-				error: false,
+		rp('http://localhost:3000/files/' + req.params.name +'.json')
+			.then((json) => {
+				console.log(json);
+				var item = JSON.parse(json);
+				item = Object.assign(
+					{}, 
+					item, 
+					{ gifUrl: 'http://localhost:3000/files/' + item.gifUrl }
+				);
+				console.log('item', item);
+				res.render('pages/index', {
+					entries: [item],
+					error: false,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				res.render('pages/index', {
+					error: true,
+				});
 			});
-		} 
-		catch(err) {
-			res.render('pages/index', {
-				error: true,
-			});
-		}
+	});
+
+	router.get('/files/:file', (req, res) => {
+		var fileName = req.params.file;
+		res.sendFile(fileName, {root: __dirname + '/../../records'}, (err) => {
+			if (err) {
+				console.log(err);
+				res.status(err.status).end();
+			}
+			else {
+				console.log('Sent:', fileName);
+			}
+	  });
 	});
 
 	router.get('/about', (req, res) => {
